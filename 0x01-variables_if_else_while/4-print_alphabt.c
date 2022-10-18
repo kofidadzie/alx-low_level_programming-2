9e2e7467aba35d441ade578aae4c9691930bdd46
kofidadzie <danielkofidadzie@gmail.com>
2022-10-17 22:56:37 -0700

    Task 5

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,24 +7,18 @@
 
 int main(void)
 {
+
 	char c;
 
-	char d;
-
 	c = 'a';
-	d = 'A';
 	while
 		(c <= 'z') {
-			putchar(c);
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
 			c++;
 		}
-	while 
-		(d <= 'Z') {
-			putchar(d);
-			dtt;
-		}
-	putchar('\n');		  
+	putchar('\n');
 	return (0);
+
 }
-			
 
